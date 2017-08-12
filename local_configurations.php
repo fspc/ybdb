650682f81ea221d6d386dafe4865360f93534ac8
Jonathan Rosenbaum <gnuser@gmail.com>
2017-08-12 03:19:42 +0000

    New configuration format file.
    * local_configurations.php allows personalized configurations for YBDB.PHP and database_functions.php
    
    * Need to fix a bug that doesn't properly parse array configuration

---
+++ local_configurations.php
@@ -0,0 +1,16 @@
+<?php
+
+// Options for YBDB.php
+
+$hostname_YBDB = "localhost";
+$database_YBDB = "ybdb";
+$username_YBDB = "admin";
+$password_YBDB = "yblcatx";
+
+// See Configurations/database_functions.php for more configuration options.
+
+define("DEFAULT_SHOP_USER", "Stand Time");
+define("DEFAULT_TRANSACTION_TYPE", "Stand Time");
+define("CHANGE_FUND", 50);
+
+?>
